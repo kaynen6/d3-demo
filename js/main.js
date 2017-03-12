d3434eb0380c1f5a00e25858cd6f6d74cfa74fc1
Kayne Neigherbauer <kayne.n@gmail.com>
2017-03-12 13:16:48 -0500

    Module 7 Assignment
    I ADDED 4 cities instead of replacing the 4 with new ones. It was more
    fun anyway.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.onload = function(){
     
     
     //svg dimension variables
-    var w = 900, h = 500;
+    var w = 1000, h = 500;
     
     //get body element from DOM
         
@@ -20,7 +20,7 @@ window.onload = function(){
     
     //inner rect block
     var innerRect = container.append("rect")
-        .datum(400) //datum value
+        .datum(450) //datum value
         .attr("width", function(d){
             return d *2; 
         }) //rect width
@@ -69,6 +69,48 @@ window.onload = function(){
         }
     ];
     
+    
+    var x = d3.scaleLinear() //create the scale
+        .range([90, 810]) //output min and max
+        .domain([0, 7]); //input min and max
+    
+    
+    //find min value of the array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+    
+    
+    //find max value of array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+    
+    var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+        .domain([
+            minPop,
+            maxPop
+        ]);
+    
+    //scaale for circles center y coordinate
+    var y = d3.scaleLinear()
+        .range([440,95])
+        .domain([0, 700000]);
+    
+    //create y axis generator 
+    var yAxis = d3.axisLeft(y);
+    
+    //create axis g element and add axis
+    var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform","translate(50,0)")
+        .call(yAxis);
+    
+    
     var circles = container.selectAll(".circles")
         .data(cityPop)
         .enter() //join
@@ -81,17 +123,61 @@ window.onload = function(){
             var area = d.population * 0.01;
             return Math.sqrt(area/Math.PI);
         })
-        .attr("cx", function(d, i){ //x coordinate
-            return 90 + (i*90);
+        .attr("cx", function(d, i){ //x coordinate via scale generator
+            return x(i);
         })
         .attr("cy", function(d){ //y coordinate
-            return 450 - (d.population *0.0005);
+            return y(d.population);
+        })
+        .style("fill", function(d,i){ //add fill color based on color scale generator
+            return color(d.population);
+        })
+        .style("stroke", "#000"); //black circle stroke
+    
+    //create text element and add the title
+    var title = container.append("text")
+        .attr("class","title")
+        .attr("text-anchor","middle")
+        .attr("x",450)
+        .attr("y", 30)
+        .text("City Populations");
+    
+    //create circle labels
+    var labels = container.selectAll(".labels")
+        .data(cityPop)
+        .enter()
+        .append("text")
+        .attr("class", "labels")
+        .attr("text-anchor", "left")
+        .attr("y",function(d){ //vertical position is centered on each circle
+            return y(d.population) - 5;
         });
     
+    //first line of label
+    var nameLine = labels.append("tspan")
+        .attr("class","nameLine")
+        .attr("x", function(d,i){
+            //horizontal position to the right of circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .text(function(d){
+            return d.city;
+        });
     
-
-    
-    
+    //create format generator
+    var format = d3.format(",");
+        
+    //second line of label
+    var popLine = labels.append("tspan")
+        .attr("class","popLine")
+        .attr("x", function(d,i){
+            //horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .attr("dy", "15")
+        .text(function(d){
+            return "Pop. " + format(d.population); //use format generator to format numbers
+        });
     
     
     
